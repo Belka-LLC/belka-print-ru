1ea5812d4aa3a7a1dd34ac0593e49afb6b2e395b
LaoShi <26747@mail.ru>
2020-07-06 16:36:17 +0300

    remove comments in templates

--- themes/bs4theme/templates/region--help.tpl.php
+++ themes/bs4theme/templates/region--help.tpl.php
@@ -1,5 +1,4 @@
 <?php if ($content) : ?>
-  <!-- region--help -->
   <div class="page__admin">
       <?php print $content; ?>
 </div>
--- themes/bs4theme/templates/region--main-navigation.tpl.php
+++ themes/bs4theme/templates/region--main-navigation.tpl.php
@@ -1,4 +1,3 @@
 <?php if ($content): ?>
-<!-- region--main-navigation -->
 <?php print $content; ?>
 <?php endif; ?>
