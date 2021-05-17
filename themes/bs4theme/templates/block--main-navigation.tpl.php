eead78f822d147dc2373114106c0eeb62104be5a
LaoShi <26747@mail.ru>
2021-05-17 12:00:09 +0300

    removed main nav h2

--- themes/bs4theme/templates/block--main-navigation.tpl.php
+++ themes/bs4theme/templates/block--main-navigation.tpl.php
@@ -1,6 +1,3 @@
 <nav class="main-nav__navbar collapse navbar-collapse" id="navbarContent">
-<?php print render($title_prefix); ?>
-<h2 class="sr-only">Основное меню</h2>
-<?php print render($title_suffix); ?>
 <?php print $content ?>
 </nav>
